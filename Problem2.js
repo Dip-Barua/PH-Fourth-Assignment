ac6ec65503123abd87894b8b537023d285f4d01e
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-09-11 15:16:51 +0600

    Solution of 2nd problem submission

---
+++ Problem2.js
@@ -0,0 +1,14 @@
+function sendNotification(email) {
+    if (email.indexOf('@') === -1 || email.split('@').length !== 2) {
+        return "Invalid Email";
+    }
+ 
+    let [username, domainname] = email.split('@');
+    
+    let notificationMessage = `${username} sent you an email from ${domainname}`;
+    
+    return notificationMessage;
+}
+
+console.log(sendNotification("zihad@gmail.com")); 
+console.log(sendNotification("sadia8icloud.com")); 
