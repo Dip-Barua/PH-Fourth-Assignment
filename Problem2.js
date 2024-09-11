070cc03274e10c310612a387f31b967ba17fc8a7
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-09-11 15:21:42 +0600

    Solution for second problem

--- Problem2.js
+++ Problem2.js
@@ -1,12 +1,12 @@
 function sendNotification(email) {
-
+    
     if (email.indexOf('@') === -1 || email.split('@').length !== 2) {
         return "Invalid Email";
     }
  
     let [username, domainname] = email.split('@');
     
-    let notificationMessage = '${username} sent you an email from ${domainname}';
+    let notificationMessage = `${username} sent you an email from ${domainname}`;
     
     return notificationMessage;
 }
